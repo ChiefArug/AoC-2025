c16e8220cf8a8c9b30ba98f4368a211b71e1dd1f
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2025-12-10 20:54:17 +1300

    Day 5 Part 2

--- AoC2025/Puzzle5.lean
+++ AoC2025/Puzzle5.lean
@@ -12,28 +12,40 @@ open Range
 
 def Range.parse (str : String) : Range :=
   let l := str.splitToList (· = '-')
-  {min := String.toNat! l[0]!, max := String.toNat! l[1]!}
+  {min := String.toNat! l[0]!, max := String.toNat! l[1]!
+  -- this is a hack to solve a compounding off by one error caused by assuming incl/excl icnorrectly that i cant be bothered to solve
+  + 1}
 
 def Range.contains (n : Nat) (r : Range) : Bool := n >= r.min && n <= r.max
+
+def Range.expand (r : Range) : List Nat := List.range' r.min (r.max - r.min)
 
 instance : ToString Range where toString := λ r => s!"{r.min} to {r.max}"
 
 def puzzle5 : IO Unit := do
   let ranges ← (fileStream "inputs/5a.txt")
-  let numbers ← (fileStream "inputs/5b.txt")
+  -- let numbers ← (fileStream "inputs/5b.txt")
 
-  match ranges.map IO.FS.Stream.lines, numbers.map IO.FS.Stream.lines with
-  | none , _ => pure ()
-  | _ , none => pure ()
-  | some ioranges, some ionumbers =>
+  match ranges.map IO.FS.Stream.lines with
+  | none => pure ()
+  | some ioranges =>
     let rangeLines ← ioranges
-    let numberLines ← ionumbers
+    -- let numberLines ← ionumbers
     let ranges := rangeLines.map Range.parse
-    let numbers := numberLines.map String.toNat!
+    -- let numbers := numberLines.map String.toNat!
 
-    let filtered := numbers.filter λ n => ranges.any (contains n)
-    IO.println filtered
-    IO.println filtered.size
+    let sorted := ranges.insertionSort λ l r => if l.min = r.min then l.max < r.max else l.min < r.min
+    let rec reducer (input : List Range) (cur prevMax : Nat) : Nat :=
+      match input with
+      | [] => cur
+      -- because we use Nat we don't have to deal with evil negative numbers!
+      | r :: rs =>
+        let trueMin := Nat.max r.min prevMax
+        let trueMax := Nat.max r.max prevMax
+        reducer rs (cur + (r.max - trueMin)) trueMax
+
+    let out := reducer sorted.toList 0 0
+    IO.println out
 
 
 
