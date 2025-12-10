599fb4d6b1246d543e4aba4fe28233bc7e432c8c
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2025-12-10 13:39:53 +1300

    Day 3 Part 2

--- AoC2025/Puzzle3.lean
+++ AoC2025/Puzzle3.lean
@@ -4,15 +4,32 @@ open Lib Option
 
 namespace Puzzle3
 
-def twoMax (cur : Char × Char) (char : Char) : Char × Char :=
-      if cur.fst < cur.snd then (cur.snd, char) else
-      if char < cur.snd then cur else
-      (if cur.snd > cur.fst then cur.snd else cur.fst, char)
+
+def shiftLeft {α : Type} (cur : Vector α 12) (new : α) : (Vector α 12) :=
+  #v[cur[1], cur[2], cur[3], cur[4], cur[5], cur[6], cur[7], cur[8], cur[9], cur[10], cur[11], new]
+
+
+
+def twelveMax (cur : Vector Char 12) (char : Char) : Vector Char 12 :=
+  if cur[0] < cur[1] then   #v[cur[1], cur[2], cur[3], cur[4], cur[5], cur[6], cur[7], cur[8], cur[9], cur[10], cur[11], char] else
+  if cur[1] < cur[2] then   #v[cur[0], cur[2], cur[3], cur[4], cur[5], cur[6], cur[7], cur[8], cur[9], cur[10], cur[11], char] else
+  if cur[2] < cur[3] then   #v[cur[0], cur[1], cur[3], cur[4], cur[5], cur[6], cur[7], cur[8], cur[9], cur[10], cur[11], char] else
+  if cur[3] < cur[4] then   #v[cur[0], cur[1], cur[2], cur[4], cur[5], cur[6], cur[7], cur[8], cur[9], cur[10], cur[11], char] else
+  if cur[4] < cur[5] then   #v[cur[0], cur[1], cur[2], cur[3], cur[5], cur[6], cur[7], cur[8], cur[9], cur[10], cur[11], char] else
+  if cur[5] < cur[6] then   #v[cur[0], cur[1], cur[2], cur[3], cur[4], cur[6], cur[7], cur[8], cur[9], cur[10], cur[11], char] else
+  if cur[6] < cur[7] then   #v[cur[0], cur[1], cur[2], cur[3], cur[4], cur[5], cur[7], cur[8], cur[9], cur[10], cur[11], char] else
+  if cur[7] < cur[8] then   #v[cur[0], cur[1], cur[2], cur[3], cur[4], cur[5], cur[6], cur[8], cur[9], cur[10], cur[11], char] else
+  if cur[8] < cur[9] then   #v[cur[0], cur[1], cur[2], cur[3], cur[4], cur[5], cur[6], cur[7], cur[9], cur[10], cur[11], char] else
+  if cur[9] < cur[10] then  #v[cur[0], cur[1], cur[2], cur[3], cur[4], cur[5], cur[6], cur[7], cur[8], cur[10], cur[11], char] else
+  if cur[10] < cur[11] then #v[cur[0], cur[1], cur[2], cur[3], cur[4], cur[5], cur[6], cur[7], cur[8], cur[9],  cur[11], char] else
+  if cur[11] < char then    #v[cur[0], cur[1], cur[2], cur[3], cur[4], cur[5], cur[6], cur[7], cur[8], cur[9],  cur[10], char] else
+  cur
+
 
 -- Returns the max two integers
-def processLine (line : String) : IO (Char × Char) := do
-  let max := line.foldl twoMax ('\u0000', '\u0000')
-  IO.println s!"Max is {max} for string {line}"
+def processLine (line : String) : IO (Vector Char 12) := do
+  let max := line.foldl twelveMax (Vector.replicate 12 '\u0000')
+  IO.println s!"Max is {max.toList} for string {line}"
   pure max
 
 def puzzle3 : IO Unit := do
@@ -24,15 +41,13 @@ def puzzle3 : IO Unit := do
     let lines := Array.toList lines
     let lines := lines.map processLine
     let lines ← unwrapIO lines
-    let lines := lines.map listFromProd
+    let lines := lines.map Vector.toList
     let lines := lines.map String.ofList
     let lines := lines.map String.toNat!
     let sum := lines.sum
     IO.println s!"Sum is {sum}"
 
 
-
-×
 
 #eval puzzle3
 end Puzzle3
