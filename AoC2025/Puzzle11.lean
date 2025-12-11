f9709f87a4ed8ad11e77194bdc6bc56ec0eb1f84
ChiefArug <73862885+ChiefArug@users.noreply.github.com>
2025-12-11 22:16:56 +1300

    Day 11 Part 2

--- AoC2025/Puzzle11.lean
+++ AoC2025/Puzzle11.lean
@@ -5,6 +5,21 @@ open Lib Option Std
 
 namespace Puzzle11
 
+abbrev MemMap := HashMap (Bool × Bool × String) Nat
+
+
+partial def loop (map : HashMap String (List String)) (encDac encFft : Bool) (s : String) : StateT (HashMap (Bool × Bool × String) Nat) Id Nat := do match s with
+    | "out" => pure (if encDac && encFft then 1 else 0)
+    | dev => do
+      let memo ← StateT.get
+      let key := (encDac, encFft, dev)
+      match memo[key]? with
+      | some result => pure result
+      | none => do
+        let mapFunc := loop map (encDac || dev = "dac") (encFft || dev == "fft")
+        let result : Nat := (← map[dev]!.mapM mapFunc).sum
+        modify fun memo => memo.insert key result
+        pure result
 
 def puzzle11 : IO Unit := do
   let inFile ← (fileStream "inputs/11.txt")
@@ -22,14 +37,8 @@ def puzzle11 : IO Unit := do
       let cons := parts[1]!.trim.splitToList (· = ' ')
       map := map.insert dev cons
 
-    --IO.println map.toArray
+    let t ← (loop map false false "svr" {})
+    IO.println t.fst
 
-    let rec walk (n : Nat) (dev : String) : Nat :=
-      if dev = "out" then 1 else
-      if n = 0 then panic "Recursed too deep!" else
-      (map[dev]!.map (walk (n - 1))).sum
-
-    IO.println (walk 1000 "you")
-
-#eval puzzle11
+#time #eval puzzle11
 end Puzzle11
